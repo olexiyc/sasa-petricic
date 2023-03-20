98e4274c4273685d8b0b92dba0669f91d079e9a0
Oleksii cherkasov <olexiyc@gmail.com>
2023-03-20 14:14:28 +0800

    1

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function changeBg(){
+    const images = [
+    'url("./images/sasha1.jpg")',  
+    'url("./images/sasha2.jpg")',
+    'url("./images/sasha3.jpg")',
+    'url(".images/sasha4.jpg")',
+    'url("./images/sasha5.jpg")',
+    'url("./images/sasha6.jpg")',
+    'url("./images/sasha7.jpg")',
+    'url("./images/sasha8.jpg")',
+]
+
+const section = document.querySelector('section');
+const bg = images[Math.floor(Math.random() * images.length)];
+section.style.backgroundImage = bg;
+
+}
+setInterval(changeBg, 2500);
